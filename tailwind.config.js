b79314c88a1d5ca74aa4d40bd438fc5f82e2feb9
Keisuke Watanuki <watanuki@ju-zen.com>
2024-03-16 22:23:46 +0900

    Add styles to the top page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			listStyleType: {
+				circle: 'circle' // `list-style-type: circle` does not exist in Tailwind's style https://github.com/tailwindlabs/tailwindcss/blob/d86fd0bb5b69c9aa5c75d4e78f8fe78969d6ff50/stubs/config.full.js#L621-L625
+			}
+		}
 	},
 	plugins: []
 };

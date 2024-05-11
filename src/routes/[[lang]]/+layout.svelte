2afdd59c0a7ced16f7e4a81048778f74abce0bfe
Keisuke Watanuki <watanuki@ju-zen.com>
2024-05-11 17:09:18 +0900

    Remove redundant code

--- src/routes/[[lang]]/+layout.svelte
+++ src/routes/[[lang]]/+layout.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import "../../app.css";
 	import { page } from "$app/stores";
 	import Footer from "$lib/components/Footer.svelte";
@@ -11,7 +11,7 @@
 	<meta name="description" content={i18nText.description} />
 	<meta property="og:description" content={i18nText.description} />
 	<meta property="og:url" content={$page.url.href} />
-	<meta property="og:image" content={`${$page.url.origin}/og-image.jpg`} />
+	<meta property="og:image" content="{$page.url.origin}/og-image.jpg" />
 	<meta property="og:type" content="website" />
 	<meta property="og:site_name" content="Kodowg" />
 	<meta name="twitter:card" content="summary_large_image" />

1d064ff0a7ec17f64e5cd586f3a67d8d746d8757
Keisuke Watanuki <watanuki.keisuke@gmail.com>
2025-01-01 21:51:24 +0900

    Simplify page links by generating from i18n keys

--- src/routes/[[lang]]/+page.svelte
+++ src/routes/[[lang]]/+page.svelte
@@ -1,10 +1,6 @@
 <script lang="ts">
-	import characterCountImage from "$lib/assets/character-count.png?enhanced";
-	import rouletteImage from "$lib/assets/roulette.png?enhanced";
-	import timerImage from "$lib/assets/timer.png?enhanced";
-
 	let { data } = $props();
-	const { lang, i18nText } = data;
+	const { lang, i18nText, pages } = data;
 </script>
 
 <svelte:head>
@@ -18,40 +14,17 @@
 		<div>
 			<p>{i18nText.description}</p>
 		</div>
-		<div class="flex flex-wrap gap-x-6 gap-y-4">
-			<a
-				href="./{lang}/character-count"
-				class="flex flex-grow flex-col items-center justify-center gap-y-2 rounded-md bg-slate-200 px-6 pb-6 pt-4 transition-colors duration-75 hover:bg-white"
-			>
-				<enhanced:img
-					alt=""
-					src={characterCountImage}
-					class="size-full h-auto max-w-64"
-				/>
-				<span class="font-bold">{i18nText.functionName.characterCount}</span>
-			</a>
-			<a
-				href="./{lang}/roulette"
-				class="flex flex-grow flex-col items-center justify-center gap-y-2 rounded-md bg-slate-200 px-6 pb-6 pt-4 transition-colors duration-75 hover:bg-white"
-			>
-				<enhanced:img
-					alt=""
-					src={rouletteImage}
-					class="size-full h-auto max-w-64"
-				/>
-				<span class="font-bold">{i18nText.functionName.roulette}</span>
-			</a>
-			<a
-				href="./{lang}/timer"
-				class="flex flex-grow flex-col items-center justify-center gap-y-2 rounded-md bg-slate-200 px-6 pb-6 pt-4 transition-colors duration-75 hover:bg-white"
-			>
-				<enhanced:img
-					alt=""
-					src={timerImage}
-					class="size-full h-auto max-w-64"
-				/>
-				<span class="font-bold">{i18nText.functionName.timer}</span>
-			</a>
+		<div
+			class="grid grid-cols-[repeat(auto-fit,minmax(theme(spacing.80),1fr))] gap-4"
+		>
+			{#each pages as path}
+				<a
+					href="./{lang}/{path}"
+					class="flex flex-grow flex-col items-center justify-center gap-y-2 rounded-md bg-slate-200 px-6 py-4 text-xl font-bold transition-colors duration-75 hover:bg-white"
+				>
+					{i18nText.functionName[path]}
+				</a>
+			{/each}
 		</div>
 	</div>
 	<div class="grid gap-y-4 rounded-md">

0d01e5a5bb3a2d3643fab4ec977563e3bff8f4c2
Keisuke Watanuki <watanuki.keisuke@gmail.com>
2025-01-02 18:13:18 +0900

    Create Header component

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	interface Props {
+		lang: "en" | "ja";
+	}
+	let { lang }: Props = $props();
+</script>
+
+<header class="flex items-center bg-white px-4 py-3 lg:px-8">
+	<a href="/{lang}" class="text-xl font-bold">Kodowg</a>
+</header>

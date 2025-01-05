c5fe1044cd693ca14de464a18475ea8b106ddd36
Keisuke Watanuki <watanuki.keisuke@gmail.com>
2025-01-05 17:38:40 +0900

    Update i18n settings

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -6,7 +6,12 @@
 </script>
 
 <header class="flex items-center justify-center border-b border-gray-200">
-	<div class="flex w-full max-w-screen-lg px-4 py-3">
+	<div class="flex w-full max-w-screen-lg justify-between px-4 py-3">
 		<a href="/{lang}" class="text-xl font-bold">Kodowg</a>
+		<div class="flex items-center gap-x-2 text-sm">
+			<a href="/ja" lang="ja" data-sveltekit-reload>日本語</a>
+			<span>/</span>
+			<a href="/en" lang="en" data-sveltekit-reload>English</a>
+		</div>
 	</div>
 </header>

cdf671cb3b9aa00b0133991375c09f0a5f1125f3
Keisuke Watanuki <watanuki.keisuke@gmail.com>
2025-01-24 01:12:30 +0900

    Change the order of links in the Header component

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -11,9 +11,9 @@
 	<div class="flex w-full max-w-screen-lg justify-between px-4 py-3">
 		<a href="/{lang}" class="text-xl font-bold">Kodowg</a>
 		<div class="flex items-center gap-x-2 text-sm">
-			<a href="/ja{path}" lang="ja">日本語</a>
-			<span>/</span>
 			<a href="/en{path}" lang="en">English</a>
+			<span>/</span>
+			<a href="/ja{path}" lang="ja">日本語</a>
 		</div>
 	</div>
 </header>

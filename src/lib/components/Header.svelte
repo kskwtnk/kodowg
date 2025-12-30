1db44424bcc61ef527b875fea8d53474af657095
Keisuke Watanuki <watanuki.keisuke@gmail.com>
2025-12-30 23:13:57 +0900

    Fix svelte/no-navigation-without-resolve rule

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,9 @@
+<script lang="ts">
+	import { resolve } from "$app/paths";
+</script>
+
 <header class="flex items-center justify-center border-b border-gray-200">
 	<div class="flex w-full max-w-screen-lg px-4 py-3">
-		<a href="/" class="text-xl font-bold">Kodowg</a>
+		<a href={resolve("/")} class="text-xl font-bold">Kodowg</a>
 	</div>
 </header>

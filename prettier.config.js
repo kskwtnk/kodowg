67efc2312151c41072baea9ea72fc7125dd01477
Keisuke Watanuki <watanuki.keisuke@gmail.com>
2026-05-05 18:35:29 +0900

    Remove ESLint and Prettier related packages

--- prettier.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import("prettier").Config} */
-const config = {
-	useTabs: true,
-	plugins: ["prettier-plugin-svelte", "prettier-plugin-tailwindcss"],
-	overrides: [{ files: "*.svelte", options: { parser: "svelte" } }],
-};
-
-export default config;
